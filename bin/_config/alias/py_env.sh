51935d03dd0a76d41a815f09ea2c7acbdc43eeec
Feng Zhou <zhoufengzd@gmail.com>
2021-05-01 21:20:05 -0700

    py_env.sh update

--- bin/_config/alias/py_env.sh
+++ bin/_config/alias/py_env.sh
@@ -3,18 +3,33 @@
 function pyenv() {
     py_env_home="$HOME/workspace/bin/svc/bin/venv/py_env"
 
+    args=$1
     venv_name=$1
-    if [ -e ${py_env_home}/${venv_name}/bin/activate ]; then
+    if [[ $args == "-m" ]]; then
+        venv_name=$2
+        cmd="python3 -m venv ${py_env_home}/${venv_name}"
+        echo $cmd && $cmd && echo "..."
         source ${py_env_home}/${venv_name}/bin/activate
-    elif [[ $venv_name == "." ]]; then
-        export PYTHONPATH=$(pwd):$PYTHONPATH
-        echo PYTHONPATH=$PYTHONPATH
+        pip install --upgrade pip > /dev/null 2>&1
+        touch ${py_env_home}/_requirements/${venv_name}.txt
+    elif [ -e ${py_env_home}/${venv_name}/bin/activate ]; then
+        source ${py_env_home}/${venv_name}/bin/activate
     else
         echo "python venv at \$HOME/workspace/env/py_env:"
         echo "-------------------------------------------"
         # echo "$(cd ${py_env_home} && ls -d */ | grep -v _requirements)"
         cd ${py_env_home} && ls -d */ | grep -v _requirements
     fi
+}
+
+function pypath() {
+    py_dir=$1
+    if [[ $venv_name == "." ]]; then
+        export PYTHONPATH=$(pwd):$PYTHONPATH
+    elif [ -d ${py_dir} ]; then
+        export PYTHONPATH=$(pwd):$PYTHONPATH
+    fi
+    echo PYTHONPATH=$PYTHONPATH
 }
 
 alias venv="if [ -e ./venv/bin/activate ]; then source ./venv/bin/activate; else python3 -m venv venv && source ./venv/bin/activate; fi"
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ wk_dir=$(pwd)
 workspace=$HOME/workspace
 
 function _mkdir() {
-    mkdir -p $workspace/git
+    mkdir -p $workspace/git/_config
 
     mkdir -p $workspace/local/test
     mkdir -p $workspace/local/tmp/downloads
@@ -24,13 +24,15 @@ function _mkdir() {
 }
 
 function _update_desktop() {
+    # build some convenient desktop links
     local desktop=$HOME/Desktop
 
     if [ ! -f $desktop/daily.md ]; then
-        touch $workspace/local/tmp/misc/daily.md
-        touch $workspace/local/tmp/misc/random.json
-        ln -s $workspace/local/tmp/misc/daily.md $desktop/daily.md
-        ln -s $workspace/local/tmp/misc/random.json $desktop/random.json
+        local links=(daily.md random.json random.sql random.py)
+        for f in ${links[@]}; do
+            touch $workspace/local/tmp/misc/$f
+            ln -s $workspace/local/tmp/misc/$f $desktop/$f
+        done
     fi
 }
 
@@ -38,17 +40,28 @@ function _update_venv() {
     ln -s $(pwd)/venv/py_env/_requirements $workspace/bin/svc/bin/venv/py_env/_requirements
 }
 
-function _update_env_alias() {
+function _link_env_alias() {
     if [ ! -e $HOME/.env ]; then
         ln -s $workspace/bin/_config/env $HOME/.env
         ln -s $workspace/bin/_config/alias $HOME/.alias
     fi
 }
 
+function _link_git_config() {
+    if [ ! -e $workspace/git/_config/gitconfig ]; then
+        local links=(gitconfig git-credentials)
+        for f in ${links[@]}; do
+            touch $HOME/.$f
+            ln -s $HOME/.$f $workspace/git/_config/$f
+        done
+    fi
+}
+
 function main() {
     _mkdir
-    _update_env_alias
-    _update_venv
+    _link_env_alias
+    # _update_venv
+    _link_git_config
     _update_desktop
 }
 
